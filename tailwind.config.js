4e7b64383170582e532b80f2aa9f81e6801eacf4
TunNandaAung <tunnandaaung@outlook.com>
2022-02-02 14:38:37 +0630

    Scaffold words section

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        quote: "#3a2837",
+        red: "#d5333e",
+      },
+    },
+  },
+  plugins: [],
+};
